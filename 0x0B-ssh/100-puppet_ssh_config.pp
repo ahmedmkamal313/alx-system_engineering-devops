845e5eed02b78a74286f770ef80359113c6ab292
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-11-24 15:41:23 -0800

    Puppet Client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,14 @@
 # 100-puppet_ssh_config.pp
 
-# Use the file resource type to manage the /etc/ssh/ssh_config file
-file { '/etc/ssh/ssh_config':
-  # Ensure that the file exists and is a regular file
-  ensure => file,
-  # Use the file_line resource type to add or modify lines in the file
-  # Use the match parameter to specify a regular expression to match the existing line
-  # Use the line parameter to specify the new line content
-  # Use the after parameter to specify where to insert the new line if the match is not found
-  # Turn off password authentication
-  file_line { 'Turn off passwd auth':
-    path  => '/etc/ssh/ssh_config',
-    match => '^#?PasswordAuthentication',
-    line  => 'PasswordAuthentication no',
-  },
-  # Declare identity file
-  file_line { 'Declare identity file':
-    path  => '/etc/ssh/ssh_config',
-    match => '^#?IdentityFile',
-    line  => 'IdentityFile ~/.ssh/school',
-    after => 'Host *',
-  },
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
