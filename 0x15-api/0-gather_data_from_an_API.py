590fb1e1fcd0754885c3d50040359d44f7431676
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2024-01-03 05:06:26 -0800

    a script that uses a REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,13 +10,16 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
     # Send a GET request to the API to get the user data
     user = requests.get(url + "users/{}".format(employee_id)).json()
-    # Send another GET request to the API to get the todos data with the employee ID as a parameter
+    # Send another GET request to the API to get the todos
+    # data with the employee ID as a parameter
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    # Use a list comprehension to filter the completed tasks and get their titles
+    # Use a list comprehension to filter completed tasks and get their titles
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    # Print the first line of the output with the employee name, number of completed tasks, and total number of tasks
+    # Print the first line of the output with the employee name,
+    # number of completed tasks, and total number of tasks
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    # Use another list comprehension to print the titles of the completed tasks with a tabulation and a space before each title
+    # Use another list comprehension to print the titles of the completed
+    # tasks with a tabulation and a space before each title
     [print("\t {}".format(c)) for c in completed]
