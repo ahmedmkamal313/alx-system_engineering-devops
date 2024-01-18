5d924f87af0a365a81c9bda44e0e1d50cffa2d67
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2024-01-17 16:00:11 -0800

    fix this

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
 # This script fix an issue in wordpress file
 
-class wordpress::fix_wp_settings {
-  file_line { 'replace-phpp-with-php-in-wp-settings':
-    path  => '/var/www/html/wp-settings.php',
-    line  => 'require_once(ABSPATH . \'wp-settings.php\');',
-    match => 'phpp',
-  },
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
